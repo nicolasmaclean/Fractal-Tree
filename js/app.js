2943d0f6462ce0595425f10b1daed11195544dd1
Nicolas Maclean <nicolas.maclean2326@gmail.com>
2019-05-12 19:19:11 -0500

    interactivity

--- js/app.js
+++ js/app.js
@@ -6,10 +6,33 @@ const c = canvas.getContext('2d');
 const sliderDiv = document.getElementById('sliderDiv');
 const slider = document.getElementById('slider');
 const sliderP = document.getElementById('sliderAmt');
+const check2 = document.getElementById('2');
+const check3 = document.getElementById('3');
+const check4 = document.getElementById('4');
+const lenInput = document.getElementById('length');
 
 //initializes on change in window size or new slider vaule
 window.addEventListener('resize', init);
-slider.addEventListener('input', refresh)
+slider.addEventListener('input', () => {
+    angle = slider.value;
+    init();
+});
+check2.addEventListener('change', () => {
+    type = 0;
+    init();
+});
+check3.addEventListener('change', () => {
+    type = 1;
+    init();
+});
+check4.addEventListener('change', () => {
+    type = 2;
+    init();
+});
+lenInput.addEventListener('input', () => {
+    minSize = lenInput.value;
+    init();
+});
 
 //stores canvas dimensions
 var width = window.innerWidth;
@@ -18,16 +41,15 @@ var height = window.innerHeight;
 //constants for tree;
 var length = canvas.height/3;
 var angle = Math.PI/4;
-const minSize = 2;
+var minSize = 10;
+var type = 0;
 
+
+lenInput.value = 10;
 function init() {
     //sets up slider;
     sliderDiv.style.width = width/2 + "px";
-    sliderDiv.style.height = "2rem";
-    
-    sliderP.innerHTML = parseFloat(slider.value).toFixed(2);
-    sliderP.style.fontSize = "1.5rem";
-    sliderP.style.color = "#fff";
+    // sliderDiv.style.height = "10rem";
     
     slider.style.width = width/4 + "px";
     slider.defaultValue = 3*Math.PI/4;
@@ -41,23 +63,32 @@ function init() {
     canvas.width = width;
     canvas.height = height;
     
-    length = canvas.height/3;
-
+    if(canvas.height < canvas.width){
+        length = canvas.height/3;
+    } else {
+        length = canvas.width/3;
+    }
+    
     //draws background color
     c.fillStyle = "#333";
     c.fillRect(0, 0, width, height);
     
+    let temp = "angle: " + (180* parseFloat(slider.value).toFixed(2)/Math.PI);
+    if(temp.substring(8, 9) === "."){
+        sliderP.innerHTML = temp.substring(0, 8) + '\u00B0';
+    } else
+        sliderP.innerHTML = temp.substring(0, 9) + '\u00B0';
+    sliderP.style.fontSize = "1.5rem";
+    sliderP.style.color = "#fff";
+
     //moves the origin of the canvas
     c.translate(width/2, height);
-
-    //draws tree
-    branch(length);
-}
-
-function refresh() {
-    init();
-    angle = slider.value;
-    //2.46 looks like snow flake fractal
+    
+    switch(type){
+        case 0: branch(length); break;
+        case 1: branch3(length); break;
+        case 2: branch4(length); break;
+    }
 }
 
 //recursively draws tree
@@ -79,7 +110,6 @@ function branch(len) {
 
         //rotates right
         c.rotate(angle);
-        // c.rotate(angle + ((Math.random()*Math.PI/8) - Math.PI/16));
 
         //recursively calls back
         branch(2*len/3);
@@ -90,44 +120,67 @@ function branch(len) {
         //does the same for the left side
         c.save();
         c.rotate(-angle);
-        // c.rotate(-angle + ((Math.random()*Math.PI/8) - Math.PI/16));
         branch(2*len/3);
         c.restore();
-
-        // c.save();
-        // branch(2*len/3);
-        // c.restore();
-
-        //adds in another 2 branches
-        // c.save();
-        // c.rotate(angle/4);
-        // branch(2*len/3);
-        // c.restore();
-
-        // c.save();
-        // c.rotate(-angle/4);
-        // branch(2*len/3);
-        // c.restore();
     }
 }
 
-//animation
-// var anim = 0;
-// var da = .01;
+function branch3(len){
+    c.strokeStyle = "#fff";
+    c.beginPath();
+    c.moveTo(0, 0);
+    c.lineTo(0, -len);
+    c.stroke();
 
-// function animate() {
-//     requestAnimationFrame(animate);
-//     c.fillStyle = "#333";
-//     c.fillRect(0, 0, width, height);
+    c.translate(0, -len);
 
-//     anim += da;
-//     // if(anim > Math.PI || anim < 0)
-//     //     da = -da;
-//     // slider.value = anim;
-//     refresh();
-// }
+    if(len > minSize){
+        c.save();
+        c.rotate(angle);
+        branch(2*len/3);
+        c.restore();
 
-//initalizes at run time
+        c.save();
+        c.rotate(-angle);
+        branch(2*len/3);
+        c.restore();
+
+        c.save();
+        branch(2*len/3);
+        c.restore();
+    }
+}
+
+function branch4(len){
+    c.strokeStyle = "#fff";
+    c.beginPath();
+    c.moveTo(0, 0);
+    c.lineTo(0, -len);
+    c.stroke();
+
+    c.translate(0, -len);
+
+    if(len > minSize){
+        c.save();
+        c.rotate(angle);
+        branch(2*len/3);
+        c.restore();
+
+        c.save();
+        c.rotate(-angle);
+        branch(2*len/3);
+        c.restore();
+
+        c.save();
+        c.rotate(angle/3);
+        branch(2*len/3);
+        c.restore();
+
+        c.save();
+        c.rotate(-angle/3);
+        branch(2*len/3);
+        c.restore();
+    }
+}
+
 init();
-
-//animate();
